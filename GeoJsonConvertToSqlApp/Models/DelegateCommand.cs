5b8884fd2bd858123c06129d92552af026c76a92
YSD)Tabeta <h-tabeta@yotsuba-system.co.jp>
2020-04-10 13:35:43 +0900

    ICommandインターフェースを継承したdelegeteクラス追加

---
+++ GeoJsonConvertToSqlApp/Models/DelegateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Input;
+
+namespace GeoJsonConvertToSqlApp.Models
+{
+    public class DelegateCommand : ICommand
+    {
+        System.Action execute;
+        System.Func<bool> canExecute;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public event System.EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+
+        public DelegateCommand(System.Action execute, System.Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+    }
+}
