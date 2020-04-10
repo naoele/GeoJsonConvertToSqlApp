ce17d0f94dbf627517504d62df905533aaae89e7
YSD)Tabeta <h-tabeta@yotsuba-system.co.jp>
2020-04-10 10:48:35 +0900

    INotifyPropertyChangedインターフェース追加

---
+++ GeoJsonConvertToSqlApp/Models/NotifyChangedBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoJsonConvertToSqlApp.Models
+{
+    abstract class NotifyChangedBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// プロパティが変更された際に発火するイベントハンドラ.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// プロパティが変更されたことを通知する.
+        /// </summary>
+        /// <param name="propertyName">プロパティ名</param>
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
